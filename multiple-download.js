9ab44ea8e30cee07feb4fbc356a334a5e76240f0
yuanl01 <yuanl01@jumei.com>
2020-06-06 15:12:33 +0800

    修复短连接解析, 更新api地址

--- multiple-download.js
+++ multiple-download.js
@@ -7,8 +7,8 @@ const taskOpen = async () => {
         '请输入QQ音乐歌单链接：',
         '歌单链接'
     );
-    const url = `https://api.ooopn.com/restore/api.php?url=${encodeURIComponent(shareUrl)}`;
-    const { data: { longUrl } } = await axiosInstance(url);
+    const url = `https://api.uomg.com/api/dwz2long?url=${encodeURIComponent(shareUrl)}`;
+    const { data: { ae_url: longUrl } } = await axiosInstance(url);
     const id = longUrl.match(/\?id=(\d+)$/i) || longUrl.match(/\/(\d+)\.html$/i);
     id ? (musicList = await getSongList(id[1])) : console.log("QQ音乐的歌单分享链接不正确!".red.bold);
     multiDownload(musicList);
